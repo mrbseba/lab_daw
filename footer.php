feb887892e47c187484b445f68daf73907bacdb0
Eusebiu <55321643+mrbseba@users.noreply.github.com>
2021-12-20 18:01:08 +0200

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+
+    <!-- footer -->
+	<div class="footer-area">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-3 col-md-6">
+					<div class="footer-box about-widget">
+						<h2 class="widget-title">Despre Noi </h2>
+						<p>Aceasta aplicație este creată cu drag de echipa noastră pentru a ușora viașa colecționerilor.</p>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-6">
+					<div class="footer-box get-in-touch">
+						<h2 class="widget-title">Contactați-ne</h2>
+						<ul>
+							<li>albot.elena@iis.utm.md</li>
+							<li>bejan.elena@iis.utm.md</li>
+							<li>barimb.eusebiu@iis.utm.md</li>
+						</ul>
+						<div class="social-icons">
+							<ul>
+								<li><a href="#" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
+								<li><a href="#" target="_blank"><i class="fab fa-instagram"></i></a></li>
+								<li><a href="#" target="_blank"><i class="fab fa-linkedin"></i></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-6">
+					<div class="footer-box pages">
+						<h2 class="widget-title">Pagini</h2>
+						<ul>
+							<li><a href="http://localhost/DAW/home/index.php">Acasă</a></li>
+							<li><a href="http://localhost/DAW/about.php">Despre Noi</a></li>
+							<li><a href="http://localhost/DAW/contact.php">Contacte</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-6">
+					<div class="footer-box subscribe">
+						<h2 class="widget-title">Înscriete la notificări</h2>
+						<p>Abonează-te la lista noastră de corespondență pentru a obține cele mai recente actualizări.</p>
+						<form action="index.php">
+							<input type="email" placeholder="Email">
+							<button type="submit"><i class="fas fa-paper-plane"></i></button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- end footer -->
+
+    <!-- jquery -->
+	<script src="http://localhost/DAW/assets/js/jquery-1.11.3.min.js"></script>
+	<!-- bootstrap -->
+	<script src="http://localhost/DAW/assets/bootstrap/js/bootstrap.min.js"></script>
+	<!-- count down -->
+	<script src="http://localhost/DAW/assets/js/jquery.countdown.js"></script>
+	<!-- isotope -->
+	<script src="http://localhost/DAW/assets/js/jquery.isotope-3.0.6.min.js"></script>
+	<!-- waypoints -->
+	<script src="http://localhost/DAW/assets/js/waypoints.js"></script>
+	<!-- owl carousel -->
+	<script src="http://localhost/DAW/assets/js/owl.carousel.min.js"></script>
+	<!-- magnific popup -->
+	<script src="http://localhost/DAW/assets/js/jquery.magnific-popup.min.js"></script>
+	<!-- mean menu -->
+	<script src="http://localhost/DAW/assets/js/jquery.meanmenu.min.js"></script>
+	<!-- sticker js -->
+	<script src="http://localhost/DAW/assets/js/sticker.js"></script>
+	<!-- main js -->
+	<script src="http://localhost/DAW/assets/js/main.js"></script>
+	</html>
+    
